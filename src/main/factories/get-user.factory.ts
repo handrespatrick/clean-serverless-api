211ae5b30193646cb74b16be8e22d992eef1a36c
handrespatrick <handrespatrick@gmail.com>
2023-08-02 17:08:37 -0300

    refactor: remove axios dependency and implement HttpAdapter

--- src/main/factories/get-user.factory.ts
+++ src/main/factories/get-user.factory.ts
@@ -1,10 +1,12 @@
 import { GetUserController } from "../../application/controllers/get-user/controller";
 import { GetUserService } from "../../application/controllers/get-user/service";
 import { GetUserFromStarwars } from "../../domain/usecases/get-user-from-starwars";
-import axios from "axios";
+import { HttpAdapter } from "../../infra/adapters/http-adapter";
 
 export const makeGetUserController = () => {
-  const getUserFromStarwars = new GetUserFromStarwars(axios);
+  const baseUrl = "https://swapi.dev/api";
+  const httpAdapter = new HttpAdapter(baseUrl);
+  const getUserFromStarwars = new GetUserFromStarwars(httpAdapter);
   const service = new GetUserService(getUserFromStarwars);
   return new GetUserController(service);
 };
