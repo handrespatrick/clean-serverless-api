04104518904ac4ae43969755367212e3a0df1829
handrespatrick <handrespatrick@gmail.com>
2023-08-02 11:41:07 -0300

    feat: implement getUserController

---
+++ src/application/controllers/get-user/controller.ts
@@ -0,0 +1,21 @@
+import { notFound, ok, serverError } from "../../helpers/http-helpers";
+import { IGetUserController } from "../../../domain/contracts/get-user/controller";
+import { IGetUserRequestDto } from "../../../domain/contracts/get-user/get-user-request-and-response-dto";
+import { HttpResponse } from "../../../domain/contracts/helpers/http-response";
+import { GetUserService } from "./service";
+
+export class GetUserController implements IGetUserController {
+  constructor(private readonly _service: GetUserService) {}
+  async handle({ name }: IGetUserRequestDto): Promise<HttpResponse> {
+    try {
+      const result = await this._service.handle(name);
+
+      if (result.notFound) {
+        return notFound(name);
+      }
+      return ok(result);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
