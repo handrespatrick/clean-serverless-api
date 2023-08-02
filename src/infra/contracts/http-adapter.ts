ee5ace4f0695cd13db5912acbb3d9547ccf22b1e
handrespatrick <handrespatrick@gmail.com>
2023-08-02 17:06:32 -0300

    feat: implement interface IHttpAdapter

---
+++ src/infra/contracts/http-adapter.ts
@@ -0,0 +1,8 @@
+import { AxiosRequestConfig } from "axios";
+
+export interface IHttpAdapter {
+  get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  post<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+  put<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+  delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+}
