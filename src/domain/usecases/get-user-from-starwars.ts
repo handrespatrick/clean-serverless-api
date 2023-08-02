d2e4f25d334aa226dfbe6d634d5393f316f77d29
handrespatrick <handrespatrick@gmail.com>
2023-08-02 17:08:21 -0300

    refactor: remove axios dependency and implement HttpAdapter

--- src/domain/usecases/get-user-from-starwars.ts
+++ src/domain/usecases/get-user-from-starwars.ts
@@ -1,27 +1,27 @@
+import { HttpAdapter } from "../../infra/adapters/http-adapter";
 import { IGetUserFromStarwars } from "../contracts/get-user/get-user-from-starwars";
 import {
-  IGetUserResponseDto,
-  IGetUserFromStarwarsResponseDto,
+  GetUserResponseDto,
+  GetUserFromStarwarsResponseDto,
 } from "../contracts/get-user/get-user-request-and-response-dto";
 
 export class GetUserFromStarwars implements IGetUserFromStarwars {
-  constructor(private readonly _axios) {}
+  constructor(private readonly _httpAdapter: HttpAdapter) {}
 
-  async handle(name: string): Promise<IGetUserResponseDto> {
-    const url = "https://swapi.dev/api/people";
+  async handle(name: string): Promise<GetUserResponseDto> {
+    let response: GetUserFromStarwarsResponseDto = await this._httpAdapter.get(
+      "/people"
+    );
 
-    let response = await this._axios.get(url);
-    let result: IGetUserFromStarwarsResponseDto = await response.data;
-
-    while (result.next) {
-      const userInfo = result.results.find((p) => p.name == name);
+    while (response.next) {
+      const userInfo = response.results.find((p) => p.name == name);
       if (userInfo) {
         console.log(userInfo);
         return { data: userInfo };
       }
 
-      response = await this._axios.get(result.next);
-      result = await response.data;
+      const parsedNextUrl = new URL(response.next).search;
+      response = await this._httpAdapter.get(`/${parsedNextUrl}`);
     }
 
     return { notFound: true };
