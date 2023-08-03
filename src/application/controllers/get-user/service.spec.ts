abdb116f65860720cea72cfba9e07bd02ec0de7b
handrespatrick <handrespatrick@gmail.com>
2023-08-03 19:23:04 -0300

    test: implement test of Service

--- src/application/controllers/get-user/service.spec.ts
+++ src/application/controllers/get-user/service.spec.ts
@@ -1,38 +1,38 @@
 import { GetUserService } from "./service";
 import { GetUserFromStarwars } from "../../../domain/usecases/get-user-from-starwars";
-import { IGetUserService } from "../../../domain/contracts/get-user/service";
-import { IGetUserFromStarwars } from "../../../domain/contracts/get-user/get-user-from-starwars";
+import { HttpAdapter } from "../../../infra/adapters/http-adapter";
 
-interface SutTypes {
-  sut: IGetUserService;
-  getUserFromStarwars: IGetUserFromStarwars;
-}
+type SutTypes = {
+  sut: GetUserService;
+  getUserFromStarwars: GetUserFromStarwars;
+  httpAdapter: HttpAdapter;
+};
 
 const makeSut = (): SutTypes => {
-  const mockAxios = {
-    get: jest.fn(),
-  };
-  const getUserFromStarwars = new GetUserFromStarwars(mockAxios);
+  const baseUrl = "http://localhost:";
+  const httpAdapter = new HttpAdapter(baseUrl);
+  const getUserFromStarwars = new GetUserFromStarwars(httpAdapter);
   const sut = new GetUserService(getUserFromStarwars);
 
   return {
     sut,
     getUserFromStarwars,
+    httpAdapter,
   };
 };
 
 describe("GetUserService", () => {
-  it("Should call handle method with the correct name", async () => {
+  it("Should call GetUserFromStarwars with the correct name", async () => {
     const name = "Luke Skywalker";
     const { sut, getUserFromStarwars } = makeSut();
 
-    jest
+    const spyGetUserFromStarwars = jest
       .spyOn(getUserFromStarwars, "handle")
       .mockResolvedValue({ data: { name: "Luke Skywalker", height: "172" } });
 
     const result = await sut.handle(name);
 
-    expect(getUserFromStarwars.handle).toHaveBeenCalledWith("Luke Skywalker");
+    expect(spyGetUserFromStarwars).toHaveBeenCalledWith("Luke Skywalker");
 
     expect(result).toEqual({ data: { name: "Luke Skywalker", height: "172" } });
   });
