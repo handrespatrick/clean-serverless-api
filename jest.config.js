6848b7d9554b12329d9aa40c9d85e20235a99286
handrespatrick <handrespatrick@gmail.com>
2024-01-18 15:22:52 -0300

    test: adding jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-node',
   verbose: true,
-};
+  setupFiles: ['./tests/setup.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.scafflater/init/test']
+}
