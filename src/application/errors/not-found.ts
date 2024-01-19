0f2a30aab8abcd4ac2f13258318b10c8cf11a344
handrespatrick <handrespatrick@gmail.com>
2024-01-19 15:13:41 -0300

    test: increasing test

--- src/application/errors/not-found.ts
+++ src/application/errors/not-found.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 export class NotFound extends Error {
   constructor(value: string) {
     super(`The name '${value}' was not found`)
--- src/application/errors/server-error.ts
+++ src/application/errors/server-error.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 export class ServerError extends Error {
   constructor(error: Error) {
     super(`Internal server error`)
