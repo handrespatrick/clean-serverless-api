9cfb6e62ed7b5b095c3f10ee8453d9cd5b34fb1b
handrespatrick <handrespatrick@gmail.com>
2024-02-09 17:11:19 -0300

    refactor: separating responsability to controller

--- src/application/usecases/get-user-from-starwars.ts
+++ src/application/usecases/get-user-from-starwars.ts
@@ -1,6 +1,5 @@
 import {
   IGetUserFromStarwars,
-  GetUserResponseDto,
   GetUserFromStarwarsResponseDto
 } from '@/domain/protocols/get-user/get-user-from-starwars'
 
@@ -9,20 +8,20 @@ import { IHttpAdapter } from '../protocols/http-adapter'
 export class GetUserFromStarwars implements IGetUserFromStarwars {
   constructor(private readonly _httpAdapter: IHttpAdapter) {}
 
-  async handle(name: string): Promise<GetUserResponseDto> {
+  async handle(name: IGetUserFromStarwars.Input): Promise<IGetUserFromStarwars.Output> {
     let response: GetUserFromStarwarsResponseDto = await this._httpAdapter.get('/people')
 
     while (response.next) {
-      const userInfo = response.results.find((p) => p.name == name)
+      const userInfo = response.results.find((person) => person.name === name)
 
       if (userInfo) {
-        return { data: userInfo }
+        return userInfo
       }
 
       const parsedNextUrl = new URL(response.next).search
       response = await this._httpAdapter.get(`/${parsedNextUrl}`)
     }
 
-    return { notFound: true }
+    return null
   }
 }
