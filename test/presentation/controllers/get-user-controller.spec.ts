424f9fcadf4b6266bd7bd99f38abd41a7b38ca4b
handrespatrick <handrespatrick@gmail.com>
2024-02-09 17:12:29 -0300

    test: improving controller

--- test/presentation/controllers/get-user-controller.spec.ts
+++ test/presentation/controllers/get-user-controller.spec.ts
@@ -47,7 +47,27 @@ describe('GetUserController', () => {
 
   it('Should return status 200 if name was found', async () => {
     const expectedUser = {
-      data: { id: 1, name: 'John Doe' }
+      name: 'John Doe',
+      height: '172',
+      mass: '77',
+      hair_color: 'blond',
+      skin_color: 'fair',
+      eye_color: 'blue',
+      birth_year: '19BBY',
+      gender: 'male',
+      homeworld: 'https://swapi.dev/api/planets/1/',
+      films: [
+        'https://swapi.dev/api/films/1/',
+        'https://swapi.dev/api/films/2/',
+        'https://swapi.dev/api/films/3/',
+        'https://swapi.dev/api/films/6/'
+      ],
+      species: [],
+      vehicles: ['https://swapi.dev/api/vehicles/14/', 'https://swapi.dev/api/vehicles/30/'],
+      starships: ['https://swapi.dev/api/starships/12/', 'https://swapi.dev/api/starships/22/'],
+      created: '2014-12-09T13:50:51.644000Z',
+      edited: '2014-12-20T21:17:56.891000Z',
+      url: 'https://swapi.dev/api/people/1/'
     }
     const { sut, useCase } = makeSut()
 
@@ -63,7 +83,7 @@ describe('GetUserController', () => {
     const name = 'John Doe'
     const { sut, useCase } = makeSut()
 
-    jest.spyOn(useCase, 'handle').mockResolvedValue({ notFound: true })
+    jest.spyOn(useCase, 'handle').mockResolvedValue(null)
 
     const response = await sut.handle(event)
 
